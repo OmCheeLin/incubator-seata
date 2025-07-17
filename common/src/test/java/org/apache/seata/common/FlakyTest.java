fc2443bccf08e7adef7468d2e50b0d5768f98991
OmCheeLin <19563671928@163.com>
2025-07-17 16:07:56 +0800

    1

--- common/src/test/java/org/apache/seata/common/FlakyTest.java
+++ common/src/test/java/org/apache/seata/common/FlakyTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.seata.flaky;
+package org.apache.seata.common;
 
 import org.junit.jupiter.api.Test;
 
